6dd9a35bf21160715c40af3b5dbe39a4f845f5c3
MelodyGhost <mgrahim.dev@gmail.com>
2022-04-09 02:41:48 +0600

    get project skeleton ready

---
+++ components/content-display/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ContentDisplay = () => {
+  return <div>ContentDisplay</div>;
+};
+
+export default ContentDisplay;
---
+++ components/content-display/tile-view/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TileView = () => {
+  return <div>TileView</div>;
+};
+
+export default TileView;
